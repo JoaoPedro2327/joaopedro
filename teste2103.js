e8129ef44554c53526beb930b06dc95d63f4fa42
JoaoPedro2327 <joao.araujo.alves10@escola.pr.gov.br>
2025-03-26 14:04:58 -0300

    Create teste2103.js

---
+++ teste2103.js
@@ -0,0 +1,17 @@
+javascript
+// Definição das variáveis
+var nome = "Gabriel Mendonça";
+var idade = 25;
+
+// Troca os valores
+nome = "Maria Silva";
+idade = 30;
+
+// Impressão no console
+console.log(
+Nome: ${nome}
+);
+console.log(
+Idade: ${idade}
+);
+
