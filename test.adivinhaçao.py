4bb0cdbc3731db16661ca9f8399770d5011d2b59
JoaoPedro2327 <joao.araujo.alves10@escola.pr.gov.br>
2025-06-09 22:42:05 -0300

    Create test.adivinhaçao.py

---
+++ test.adivinhaçao.py
@@ -0,0 +1,23 @@
+Escolha o nível de dificuldade (1- Fácil, 2- Médio, 3- Difícil): 2
+Jogo de Adivinhação - Nível 2
+Tente adivinhar o número que estou pensando, entre 1 e 50.
+Tentativa 1 de 5
+Digite seu palpite: 5
+Seu palpite foi menor do que o número secreto.
+Tentativa 2 de 5
+Digite seu palpite: 12
+Seu palpite foi menor do que o número secreto.
+Tentativa 3 de 5
+Digite seu palpite: 41
+Seu palpite foi maior do que o número secreto.
+Tentativa 4 de 5
+Digite seu palpite: 28
+Seu palpite foi maior do que o número secreto.
+Tentativa 5 de 5
+Digite seu palpite: 19
+Seu palpite foi menor do que o número secreto.
+Suas tentativas acabaram. O número secreto era 24.
+Fim do jogo!
+
+...Program finished with exit code 0
+Press ENTER to exit console.
