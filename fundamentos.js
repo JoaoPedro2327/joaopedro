de4d33ffac5316bdbfd14fa31660443b6974c3b2
JoaoPedro2327 <joao.araujo.alves10@escola.pr.gov.br>
2025-09-17 22:12:12 -0300

    Create fundamentos.js

---
+++ fundamentos.js
@@ -0,0 +1,37 @@
+// ================================
+// Exercício 9 - Variável idade
+// ================================
+let idade = 30;
+console.log(idade);
+
+
+
+/* ================================
+   Exercício 10 - Função soma
+   ================================ */
+function soma(a, b) {
+  return a + b;
+}
+console.log(soma(5, 7));
+
+
+
+/* ================================
+   Exercício 11 - Par ou Ímpar
+   ================================ */
+let num = 7;
+
+if (num % 2 === 0) {
+  console.log("O número é par");
+} else {
+  console.log("O número é ímpar");
+}
+
+
+
+/* ================================
+   Exercício 12 - Loop A até Z
+   ================================ */
+for (let i = 65; i <= 90; i++) {
+  console.log(String.fromCharCode(i));
+}
