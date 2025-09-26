f22bdfae648f5233c0efed78ef57980291635eaa
JoaoPedro2327 <joao.araujo.alves10@escola.pr.gov.br>
2025-09-26 15:04:51 -0300

    Create Laços-repetição.py

---
+++ Laços-repetição.py
@@ -0,0 +1,12 @@
+# ---------------------------
+# 1. Contagem do tempo até o fim de semana (Loop for)
+print("Exercício 1 - Contagem do tempo até o fim de semana")
+dias_uteis = ["Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira"]
+
+for dia in dias_uteis:
+    print(f"Faltam alguns dias... Hoje é {dia}")
+
+print("Finalmente, chegou o fim de semana! 🎉\n")
+
+# ---------------------------
+# 4. Cálc
