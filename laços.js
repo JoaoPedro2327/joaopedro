89caaf6519b3746b8cbcc57556045a8396e2d07c
JoaoPedro2327 <joao.araujo.alves10@escola.pr.gov.br>
2025-10-06 20:07:02 -0300

    Create laços.js

---
+++ laços.js
@@ -0,0 +1,16 @@
+// ============================
+// 🧩 Desafio 1 – Tamanho da palavra
+// ============================
+
+let palavra = "";
+
+while (palavra.length <= 8) {
+  palavra = prompt("Digite uma palavra com mais de 8 letras:");
+}
+
+console.log("✅ Palavra aceita:", palavra);
+
+
+// ============================
+// 🧩 Desafio 2 – Soma de números aleatórios
+// ============================
